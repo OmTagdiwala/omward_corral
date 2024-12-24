78ac3b3c47e6e76a5b085ee329d1c234633bc374
OmTagdiwala <om.tagdiwala@gmail.com>
2024-12-24 03:25:18 -0800

    encryption and decryption works!!!

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # initalizations
 import tkinter
 import os
+from cryptography.fernet import Fernet
 loginwin = tkinter.Tk()
 
 path = __file__
@@ -8,26 +9,48 @@ path = path.replace("main.py", "userpasswords")
 # change the directory to the userpasswords folder
 os.chdir(path)
 
-'''def does_user_exist():
-    if os.path.exists(path):
-        print("exisiting user")
+def genkey():
+    if not os.path.exists("xxx.txt"):
+        key = Fernet.generate_key()
+        with open("xxx.txt", "wb") as key_file:
+            key_file.write(key)
     else:
-        print("new user detected")'''
+        with open("xxx.txt", "rb") as key_file:
+            key = key_file.read()
+    cs = Fernet(key)
+    return cs
 
 # when called this function will work (tested)
 def newuser(name, passward):
-    with open(name + ".txt", "a") as fil:
-        fil.write(passward)
-        fil.write("\n")
+    with open(name + ".txt", "wb") as fil:
+        fil.write((encryptor(passward)))
 
 def checkuserexist(name):
     if os.path.isfile(f"./{name}.txt"):
+        # only for testing
+        with open(name + ".txt", "rb") as fil:
+            q = fil.read()
+        print(decryptor(q))
         return True
     else:
         return False
     
+def encryptor(password):
+    epassword = password.encode('utf-8')
+    ecpass = genkey().encrypt(epassword)
+    return ecpass
+
+def decryptor(password):
+    dpassword = genkey().decrypt(password)
+    dcpass = dpassword.decode('utf-8')
+    return dcpass
+
+if not checkuserexist("omward"):
+    newuser("omward", "quackquack")
+else:
+    print("User already exists")
 # main login window loop
-while True:
+'''while True:
     loginwin.title("Login Window")
     loginwin.geometry("400x400")
     loginwin.resizable(True, False)
@@ -35,4 +58,4 @@ while True:
     # background image
     # bg = tkinter.PhotoImage(file="bg.png")
     loginwin.iconbitmap("omward_corral.ico")
-    loginwin.mainloop()
+    loginwin.mainloop()'''
